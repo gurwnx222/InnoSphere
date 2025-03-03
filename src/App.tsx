deae6e30f403463cd4e744ef4d3e2d740ac630b1
Kevin own <36642569-kevinowns574@users.noreply.replit.com>
2025-03-03 10:59:51 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="flex justify-center items-center bg-black h-full w-full">
+        <h1 className="font-bold text-2xl text-white">
+          React, Typescript, Tailwind!
+        </h1>
+      </div>
+    </div>
+  );
+}
